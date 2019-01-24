28a4e1f5e72715f191b88818080e0a8406b7138b
Antheny Wells <anthenywells@Go-away.local>
2019-01-23 20:47:32 -0500

    Secondary commit. JS Game Part 2 completed

--- script.js
+++ script.js
@@ -5,32 +5,58 @@ let playerHealth = 40;
 let grantHealth = 10;
 let playerWins = 0;
 let grantWins = 0;
-let game = prompt("Do you want to play a game?", "yes or no");
+let game = undefined;
+let battle = undefined;
 
-if (game === "no") {
-    console.log("Okay, maybe some other time.");
-} else {
-    user = prompt("Choose your username")
-    while (grantWins < 1 && playerWins < 3) {
-        if (playerHealth > 0 && grantHealth > 0 ) {
-             playerHealth -= Math.floor(Math.random() * 2) + 1;
-             grantHealth -= Math.floor(Math.random() * 2) + 1;
-             console.log(`${user} was attacked. Remaining health: ${playerHealth} 
-             Grant was attacked. Remaining health: ${grantHealth}`);
-        } else if (grantHealth <= 0) {    
-             playerWins += 1;
-             console.log(`You have ${playerWins} wins.`);
-             grantHealth = 10;
-        } else {
-             grantWins += 1;
-        }
-     }
-     
-     if (playerWins === 3) {
-         console.log(`${user} wins!, GAME OVER`);
-     } else {
-         console.log(`Grant wins, GAME OVER`);
-     }
+
+
+
+function startGame() {
+    game = prompt("Do you want to play a game?", "yes or no");
+    if (game === "yes") {
+        user = prompt("Choose your username");
+        startCombat();
+    } else {
+        console.log("Okay, maybe some other time.");
+    }
+    return "Goodbye.";
 }
 
+function startCombat() {
+    while (grantWins < 1 && playerWins < 3) {
+        if (playerHealth > 0 && grantHealth > 0 ) {
+            battle = prompt("Would you like to attack or quit?");
+            if (battle === "attack") {
+               let userDamage = getDamage();
+               let grantDamage = getDamage();
+                playerHealth -= userDamage;
+                grantHealth -= grantDamage;
+                console.log(`${user} was attacked and suffered ${userDamage} damage. Remaining health: %c${playerHealth}`, "color: green"); 
+                console.log(`Grant was attacked and suffered ${grantDamage} damage. Remaining health: %c${grantHealth}`, "color:red");
+            } 
+            else {
+                return "Bye."
+            }
+        } 
+        else if (grantHealth <= 0) {    
+            playerWins += 1;
+            console.log(`You have ${playerWins} wins.`);
+            grantHealth = 10;
+        } 
+        else {
+            grantWins += 1;
+        } 
+        if (playerWins === 3) {
+            console.log(`${user} wins!, GAME OVER`);
+            break;
+        } else if (grantWins === 1) {
+            console.log(`Grant wins, GAME OVER`);
+            break;
+        }
+    }
+}
 
+function getDamage() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+ 
